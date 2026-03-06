135c8c9f1d9b1403d2dfc9684d745d148e33743d
izzAndeyyyy <andreasschwarz123@outlook.com>
2026-03-06 10:24:35 +0000

    daily config backup

--- scripts/backup.py
+++ scripts/backup.py
@@ -19,7 +19,7 @@ for device in devices:
             password=""
         )
 
-        config = connection.send_command("/export", expect_string=r"#", delay_factor=4)
+        config = connection.send_command_timing("/export")
 
         filename = f"{backup_dir}/{device['name']}_{datetime.now().date()}.cfg"
 
